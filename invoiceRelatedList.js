49f3ad4a9ad2668df254df4f1a85f76cc40458ec
SrivardhanMadishetty <110886742+SrivardhanMadishetty@users.noreply.github.com>
2022-08-23 19:03:57 +0530

    Update invoiceRelatedList.js

--- invoiceRelatedList.js
+++ invoiceRelatedList.js
@@ -1,13 +1,13 @@
 import { LightningElement,api,track,wire } from 'lwc';
 import getAllInvoices from '@salesforce/apex/ListOfAllInvoicesController.getAllInvoices';
-import PaidInvoices from '@salesforce/apex/ListOfAllInvoicesController.PaidInvoices';
+import paidInvoices from '@salesforce/apex/ListOfAllInvoicesController.paidInvoices';
 import CurrentDueInvoices from '@salesforce/apex/ListOfAllInvoicesController.CurrentDueInvoices';
 import OverdueInvoices from '@salesforce/apex/ListOfAllInvoicesController.OverdueInvoices';
 import TotalReceivables from '@salesforce/apex/ListOfAllInvoicesController.TotalReceivables';
-import Red from '@salesforce/resourceUrl/Red';
 export default class InvoiceSummary extends LightningElement {
     @api recordId;
     @api allInvoices;
+    @api overDueInvoices;
     invdata=[];
 
    
@@ -16,50 +16,42 @@ export default class InvoiceSummary extends LightningElement {
         label: 'Invoice#',
         fieldName: 'invName',
         type: 'url',
-        sortable: true,
         typeAttributes: {label: { fieldName: 'Name' }, target: '_blank'}
     },
     {
         label: 'Status',
         fieldName: 'Status__c',
         type: 'image',
-        sortable: true
     },
     {
         label: 'Inoice Date',
         fieldName: 'Invoice_Date__c',
         type: 'Date',
-        sortable: true
     },
     {
         label: 'Due Date',
         fieldName: 'Due_Date__c',
         type: 'Date',
-        sortable: true
     },
     {
         label: 'Amount Paid',
         fieldName: 'Amount_Paid__c',
         type: 'currency',
-        sortable: true
     },
     {
         label: 'Amount Due',
         fieldName: 'Amount_Due__c',
         type: 'currency',
-        sortable: true
     },
     {
         label: 'Days Overdue',
         fieldName: 'Days_Overdue__c',
         type: 'number',
-        sortable: true
     },
     { 
         label: 'Total',
         fieldName: 'Total__c',
         type: 'currency',
-        sortable: true
     }
     
 ];
@@ -91,28 +83,53 @@ wiredInvoices({ error, data })
 }
 
 
-@wire(PaidInvoices,  { AccountId:'$recordId' }) totalPaidInvoices;
+@wire(paidInvoices,  { AccountId:'$recordId' }) totalPaidInvoices;
 
 @wire(CurrentDueInvoices,  { AccountId:'$recordId' }) totalDueInvoices;
 
-@wire(OverdueInvoices,  { AccountId:'$recordId' }) totoverDueInvoices;
-/*wiredInvoice({ error, data })
+ obj=[];
+ countOverdue1to30 = 0;
+ countOverdue31to60 = 0;
+ countOverdue61to90 = 0;
+ countOverdue90Above = 0;
+ totalcountOverdue = 0;
+
+@wire(OverdueInvoices,  { AccountId:'$recordId' })
+wiredInvoice({ error, data })
 {
     if (data) 
     {
-        console.log(data.status__c);
-        let totoverDueInvoices;
-        if(data>=1 && data<=30)
-        {
-            this.totoverDueInvoices = '1-30';
-        }
-              
+
+    this.obj=data;
+    
+       for(let i=0; i<this.obj.length; i++)   
+       {
+         if(this.obj[i].Days_Overdue__c >=1 && this.obj[i].Days_Overdue__c <= 30)
+         {
+                this.countOverdue1to30 = this.countOverdue1to30 + 1;
+         }
+         else if(this.obj[i].Days_Overdue__c >=31 && this.obj[i].Days_Overdue__c <= 60)
+         {
+            this.countOverdue31to60 = this.countOverdue31to60 + 1;
+         }
+         else if(this.obj[i].Days_Overdue__c >=61 && this.obj[i].Days_Overdue__c <= 90)
+         {
+            this.countOverdue61to90 = this.countOverdue61to90 + 1;
+         }
+         else if(this.obj[i].Days_Overdue__c >=91)
+         {
+            this.countOverdue90Above = this.countOverdue90Above + 1;
+         }
+       }
+       
+    this.totalcountOverdue = this.obj.length;
+
     }
     else if (error) 
     {
         this.error = result.error;
     }
-} */
+} 
 
 @wire(TotalReceivables,  { AccountId:'$recordId' }) sumOfReceivables;
 
