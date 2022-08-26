f637bc1c3b5912eeb0d8d582204b765e9a08b202
SrivardhanMadishetty <110886742+SrivardhanMadishetty@users.noreply.github.com>
2022-08-26 18:32:17 +0530

    Update invoiceRelatedList.js

--- invoiceRelatedList.js
+++ invoiceRelatedList.js
@@ -1,10 +1,18 @@
 import { LightningElement,api,track,wire } from 'lwc';
-import getAllInvoices from '@salesforce/apex/ListOfAllInvoicesController.getAllInvoices';
-import paidInvoices from '@salesforce/apex/ListOfAllInvoicesController.paidInvoices';
-import CurrentDueInvoices from '@salesforce/apex/ListOfAllInvoicesController.CurrentDueInvoices';
-import OverdueInvoices from '@salesforce/apex/ListOfAllInvoicesController.OverdueInvoices';
-import TotalReceivables from '@salesforce/apex/ListOfAllInvoicesController.TotalReceivables';
-import Red from '@salesforce/resourceUrl/Red';
+
+import chartjs from '@salesforce/resourceUrl/ChartJs'; 
+import { loadScript } from 'lightning/platformResourceLoader';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+import getAllOverdueInvoices from '@salesforce/apex/invoiceRelatedListController.getAllOverdueInvoices';
+
+import getAllInvoices from '@salesforce/apex/invoiceRelatedListController.getAllInvoices';
+
+import paidInvoices from '@salesforce/apex/invoiceRelatedListController.paidInvoices';
+import CurrentDueInvoices from '@salesforce/apex/invoiceRelatedListController.CurrentDueInvoices';
+import totalOverdueInvoices from '@salesforce/apex/invoiceRelatedListController.totalOverdueInvoices';
+import TotalReceivables from '@salesforce/apex/invoiceRelatedListController.TotalReceivables';
+
 export default class InvoiceSummary extends LightningElement {
     @api recordId;
     @api allInvoices;
@@ -105,50 +113,94 @@ wiredInvoices({ error, data })
 
 @wire(CurrentDueInvoices,  { AccountId:'$recordId' }) totalDueInvoices;
 
- obj=[];
- countOverdue1to30 = 0;
- countOverdue31to60 = 0;
- countOverdue61to90 = 0;
- countOverdue90Above = 0;
- totalcountOverdue = 0;
-
-@wire(OverdueInvoices,  { AccountId:'$recordId' })
-wiredInvoice({ error, data })
-{
-    if (data) 
-    {
-
-    this.obj=data;
-    
-       for(let i=0; i<this.obj.length; i++)   
-       {
-         if(this.obj[i].Days_Overdue__c >=1 && this.obj[i].Days_Overdue__c <= 30)
-         {
-                this.countOverdue1to30 = this.countOverdue1to30 + 1;
-         }
-         else if(this.obj[i].Days_Overdue__c >=31 && this.obj[i].Days_Overdue__c <= 60)
-         {
-            this.countOverdue31to60 = this.countOverdue31to60 + 1;
-         }
-         else if(this.obj[i].Days_Overdue__c >=61 && this.obj[i].Days_Overdue__c <= 90)
-         {
-            this.countOverdue61to90 = this.countOverdue61to90 + 1;
-         }
-         else if(this.obj[i].Days_Overdue__c >=91)
-         {
-            this.countOverdue90Above = this.countOverdue90Above + 1;
-         }
-       }
-       
-    this.totalcountOverdue = this.obj.length;
-
-    }
-    else if (error) 
-    {
-        this.error = result.error;
-    }
-} 
-
 @wire(TotalReceivables,  { AccountId:'$recordId' }) sumOfReceivables;
 
+@wire(totalOverdueInvoices,  { AccountId:'$recordId' }) totalOverdueInvoices;
+
+obj=[];
+totalcountOverdue = 0;
+
+@wire (getAllOverdueInvoices, { AccountId:'$recordId' }) 
+accounts({error,data})
+{
+   if(data)
+   {
+      this.obj =data;
+      this.totalcountOverdue = this.obj.length;
+      
+      for(var key in data)
+       {
+          this.updateChart(data[key].count,data[key].label);
+       }
+      this.error=undefined;
+   }
+  else if(error)
+  {
+     this.error = error;
+     this.accounts = undefined;
+  }
+}
+chart;
+chartjsInitialized = false;
+config={
+type : 'doughnut',
+data :{
+datasets :[
+{
+data: [
+],
+backgroundColor :[
+    'rgb(255,99,132)',
+    'rgb(255,159,64)',
+    'rgb(255,205,86)',
+    'rgb(75,192,192)',
+],
+   label:'Dataset 1'
+}
+],
+labels:[]
+},
+options: {
+    responsive : true,
+legend : {
+    position :'right'
+},
+animation:{
+   animateScale: true,
+   animateRotate : true
+}
+}
+};
+renderedCallback()
+{
+   if(this.chartjsInitialized)
+  {
+   return;
+  }
+  this.chartjsInitialized = true;
+  Promise.all([
+   loadScript(this,chartjs)
+  ]).then(() =>{
+    const ctx = this.template.querySelector('canvas.donut')
+    .getContext('2d');
+    this.chart = new window.Chart(ctx, this.config);
+  })
+  .catch(error =>{
+    this.dispatchEvent(
+    new ShowToastEvent({
+    title : 'Error loading ChartJS',
+    message : error.message,
+    variant : 'error',
+   }),
+  );
+});
+}
+updateChart(count,label)
+{
+   this.chart.data.labels.push(label);
+   this.chart.data.datasets.forEach((dataset) => {
+   dataset.data.push(count);
+   });
+   this.chart.update();
+ }
 }
